6ec1e641f00e9475a29c1cbc036a7141a0f27ebf
66160264Wittawat <66160264@go.buu.ac.th>
2025-02-19 20:33:55 +0700

    หน้ารายชื่อผู้ใช้งาน

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("user").innerHTML = data
+            .map(
+                (u) =>
+                    `<div class="container">
+                              <a class="text-a" href="user-detail.html?id=${u.id}">
+                                    ${u.name}<br>
+                                    ${u.email}
+                              </a>
+                        </div>`
+            ).join("");
+    });
